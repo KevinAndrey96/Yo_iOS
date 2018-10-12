eb13921f369796a756d2fe6e0a423d0198747dad
Kevin Andrey Herrera Silva <andrey@Andreys-Mac.local>
2018-10-12 15:50:15 -0700

    First Commit

---
+++ Yo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Yo
+//
+//  Created by Kevin Andrey Herrera Silva on 10/12/18.
+//  Copyright © 2018 Trantec. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
